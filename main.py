e34fab0c7591717a0ed95fe957ab31969109af31
Arman Kazi <80411035+Hacker6937@users.noreply.github.com>
2023-03-22 21:48:04 +0000

    improve algorithm

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 import time
 nltk.download('vader_lexicon')
 print("note: This program is not fully refined if you feel uncertain about a loved one please call the police and make sure they are safe")
-time.sleep(15)
+time.sleep(5)
 def get_sentiment(text):
     sia = SentimentIntensityAnalyzer()
     sentiment = sia.polarity_scores(text)
@@ -16,11 +16,11 @@ input_text = str(input("Please type what the loved one said that you are unsure 
 sia = SentimentIntensityAnalyzer()
 sentiment = sia.polarity_scores(input_text)
 
-num = sentiment['neg']
+num = sentiment['neg']-sentiment['neu']
 
 sentiment_value = get_sentiment(input_text)
 print(f"Overall Sentiment: {sentiment_value}")
-if num > 0.050:
+if num > 0.150:
   print("suicide possiblity, please contact loved one or officials")
 else:
   print("suicide not detected but keep close watch if uncertain")
