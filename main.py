ea58b9e84e384f4b06d5804ce9fbd945e2b4a733
Arman Kazi <80411035+Hacker6937@users.noreply.github.com>
2023-03-23 23:04:33 +0000

    updated

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import time
+
 nltk.download('vader_lexicon')
-print("Disclaimer: This program is not fully refined. If you feel uncertain about the safety of a loved one, please contact the authorities to ensure their well-being. The purpose of this program is for users to input comments from their family members that they find suspicious. The program will then analyze the statement and determine if it indicates suicidal tendencies. By using this program, you agree that any harm caused by it is not the fault of its creator, contributors or Replit.com.")
+print(
+  "Disclaimer: This program is not fully refined. If you feel uncertain about the safety of a loved one, please contact the authorities to ensure their well-being. The purpose of this program is for users to input comments from their family members that they find suspicious. The program will then analyze the statement and determine if it indicates suicidal tendencies. By using this program, you agree that any harm caused by it is not the fault of its creator, contributors or Replit.com."
+)
 time.sleep(5)
+
+
 def get_sentiment(text):
-    sia = SentimentIntensityAnalyzer()
-    sentiment = sia.polarity_scores(text)
-    print(f"Positive: {sentiment['pos']}")
-    print(f"Negative: {sentiment['neg']}")
-    print(f"Neutral: {sentiment['neu']}")
-    return sentiment['compound']
-input_text = str(input("Please enter the statement or note from your loved one that you find concerning: "))
+  sia = SentimentIntensityAnalyzer()
+  sentiment = sia.polarity_scores(text)
+  print(f"Positive: {sentiment['pos']}")
+  print(f"Negative: {sentiment['neg']}")
+  print(f"Neutral: {sentiment['neu']}")
+  return sentiment['compound']
+
+
+input_text = str(
+  input(
+    "Please enter the statement or note from your loved one that you find concerning: "
+  ))
 
 sia = SentimentIntensityAnalyzer()
 sentiment = sia.polarity_scores(input_text)
 
-num = sentiment['neg']-sentiment['neu']
+num = sentiment['neg'] - sentiment['neu']
 ov = sentiment['compound']
 sentiment_value = get_sentiment(input_text)
 print(f"Overall Sentiment: {sentiment_value}")
