45b44e88fd61a95d1387768c2fbfbc9af691e14c
Arman Kazi <80411035+Hacker6937@users.noreply.github.com>
2023-03-24 00:31:15 +0000

    revamped

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
-import time
 
 nltk.download('vader_lexicon')
 print(
-  "Disclaimer: This program is not fully refined. If you feel uncertain about the safety of a loved one, please contact the authorities to ensure their well-being. The purpose of this program is for users to input comments from their family members that they find suspicious. The program will then analyze the statement and determine if it indicates suicidal tendencies. By using this program, you agree that any harm caused by it is not the fault of its creator, contributors or Replit.com."
+  "Disclaimer: This program is not fully refined. If you feel uncertain about the safety of a loved one, please contact the authorities to ensure their well-being. The purpose of this program is for users to input comments from their family members that they find suspicious. The program will then analyze the statement and determine if it indicates suicidal tendencies. By using this program, you agree that any harm caused by it is not the fault of its creator, contributors or replit.com."
 )
-time.sleep(5)
-
 
 def get_sentiment(text):
-  sia = SentimentIntensityAnalyzer()
-  sentiment = sia.polarity_scores(text)
-  print(f"Positive: {sentiment['pos']}")
-  print(f"Negative: {sentiment['neg']}")
-  print(f"Neutral: {sentiment['neu']}")
-  return sentiment['compound']
-
+    if not text:
+        print("Error: Please enter a valid input.")
+        return None
+    sia = SentimentIntensityAnalyzer()
+    sentiment = sia.polarity_scores(text)
+    sentiment_scores = {
+        'Positive': sentiment['pos'],
+        'Negative': sentiment['neg'],
+        'Neutral': sentiment['neu'],
+        'Overall': sentiment['compound']
+    }
+    return sentiment_scores
 
 input_text = str(
   input(
     "Please enter the statement or note from your loved one that you find concerning: "
   ))
 
-sia = SentimentIntensityAnalyzer()
-sentiment = sia.polarity_scores(input_text)
+sentiment_scores = get_sentiment(input_text)
+if sentiment_scores:
+    for key, value in sentiment_scores.items():
+        print(f"{key} Sentiment: {value}")
 
-num = sentiment['neg'] - sentiment['neu']
-ov = sentiment['compound']
-sentiment_value = get_sentiment(input_text)
-print(f"Overall Sentiment: {sentiment_value}")
-if num > 0.150:
-  print("suicide possiblity, please contact officials")
-elif ov < -0.5:
-  print("suicide possiblity, please contact officials")
-else:
-  print("suicide not detected but keep close watch if uncertain")
+    num = sentiment_scores['Negative'] - sentiment_scores['Neutral']
+    ov = sentiment_scores['Overall']
+
+    if num > 0.150:
+      print("There is a possibility of suicidal tendencies. Please contact officials for assistance.")
+    elif ov < -0.5:
+      print("There is a possibility of suicidal tendencies. Please contact officials for assistance.")
+    else:
+      print("Suicidal tendencies were not detected but keep close watch if uncertain.")
